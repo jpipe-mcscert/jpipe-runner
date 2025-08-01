721d95cf36800b9af5c9082b102e46a3ffb04d7f
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2025-08-01 14:14:41 -0400

    feat(formula): remove commented line and improve test messages

--- Formula/homebrew_formula_gui_template.rb
+++ Formula/homebrew_formula_gui_template.rb
@@ -1,30 +1,30 @@
 require "formula"
 
 class $CLASS_NAME < Formula
-  include Language::Python::Virtualenv
+    include Language::Python::Virtualenv
 
-  homepage "$HOMEPAGE_URL"
-  url "$SOURCE_URL"
-  sha256 "$SOURCE_SHA256"
+    homepage "$HOMEPAGE_URL"
+    url "$SOURCE_URL"
+    sha256 "$SOURCE_SHA256"
 
-  depends_on "python@$PYTHON_VERSION"
-  depends_on "python-tk@$PYTHON_VERSION"
-  depends_on "libjpeg-turbo"
-  depends_on "freetype"
-  depends_on "graphviz"
+    depends_on "python@$PYTHON_VERSION"
+    depends_on "python-tk@$PYTHON_VERSION"
+    depends_on "libjpeg-turbo"
+    depends_on "freetype"
+    depends_on "graphviz"
 
-  $RESOURCES
+    $RESOURCES
 
-  def install
-   ENV["SOURCE_DATE_EPOCH"] = Time.now.to_i.to_s
-   virtualenv_install_with_resources
-  end
+    def install
+    ENV["SOURCE_DATE_EPOCH"] = Time.now.to_i.to_s
+    virtualenv_install_with_resources
+    end
 
-  def post_install
-    ohai "Verifying installation..."
-    system bin/"jpipe-runner", "--help"
-    ohai "should be working"
-  rescue
-    odie "will not work. have you updated python deps? RTFM."
-  end
+    def post_install
+      ohai "Verifying #{name} installation by checking command availability..."
+      system bin/"jpipe-runner", "--help"
+      ohai "#{name} installation verified successfully"
+    rescue
+      odie "#{name} verification failed. Please check Python dependencies and ensure all requirements are installed correctly."
+    end
 end
