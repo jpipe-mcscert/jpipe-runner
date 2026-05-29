93c5ca1fc9f2a6ea5ebfa1dbfcbb59092ff23df9
Veillard Corentin <corentin.veillard@etu.unice.fr>
2026-05-29 17:51:02 +0200

    test: add simple first end-to-end integration tests for GitHub Actions #88

---
+++ tests/action/resources/simple_success/file_validation.py
@@ -0,0 +1,19 @@
+import os
+
+from jpipe_runner.framework.decorators.jpipe_decorator import jpipe
+from jpipe_runner.framework.decorators.link_decorator import jpipe_link
+
+
+@jpipe_link("E1")
+@jpipe(consume=["file_path"], produce=["file_exists"])
+def check_file_exists(file_path: str, produce) -> bool:
+    """Check if a file exists"""
+    exists = os.path.exists(file_path)
+    produce("file_exists", exists)
+    return exists
+
+
+@jpipe(consume=["file_exists"])
+def file_is_valid(file_exists: bool) -> bool:
+    """Validate that file exists"""
+    return file_exists
