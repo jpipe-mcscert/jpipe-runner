3ccd8d609aeb85247737f697885da1ebbd912eed
Veillard Corentin <corentin.veillard@etu.unice.fr>
2026-06-02 17:32:29 +0200

    test: refactor GitHub Action tests to use a reusable workflow template #88

---
+++ tests/action/resources/missing_consumer/math_operation.py
@@ -0,0 +1,26 @@
+from jpipe_runner.framework.decorators.jpipe_decorator import jpipe
+from jpipe_runner.framework.decorators.link_decorator import jpipe_link
+
+
+@jpipe_link("E1")
+@jpipe(produce=["number_a"])
+def generate_number_a(produce) -> bool:
+    """Generate first number"""
+    produce("number_a", 10)
+    return True
+
+
+@jpipe_link("E2")
+@jpipe(produce=["number_b"])
+def generate_number_b(produce) -> bool:
+    """Generate second number"""
+    produce("number_b", 5)
+    return True
+
+
+# Intentionally consumes a variable that has no producer
+@jpipe_link("S1")
+@jpipe(consume=["missing_var"])
+def add_numbers(missing_var: int) -> bool:
+    """Add two numbers - fails due to missing variable"""
+    return missing_var > 0
